1aff8360a17d75e29d11ff2dfa292bbb08181ae9
Paul-Louis NECH <pln@algolia.com>
2025-05-01 21:36:45 +0200

    chore: mem5

--- src/models.py
+++ src/models.py
@@ -141,7 +141,7 @@ class Agent(BaseModel):
     goods: list[Good] = Field(default_factory=list)
 
     history: list[tuple[float, AgentNeeds, list[Good], "AgentActionResponse"]] = Field(default_factory=list)
-    memory: int = 3
+    memory: int = 5
 
     def record(self, action: "AgentActionResponse"):
         self.history.append((deepcopy(self.credits), deepcopy(self.needs), deepcopy(self.goods), action))
